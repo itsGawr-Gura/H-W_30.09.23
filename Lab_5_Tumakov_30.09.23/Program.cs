baeaad88d399ec824202f09bfdf8635d7a7d81b5
alesi <alesi@LAPTOP-OB7OP2I0>
2023-10-05 15:04:01 +0300

    Добавьте файлы проекта.

---
+++ Lab_5_Tumakov_30.09.23/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_Tumakov_30._09._23
+{
+    internal class Program
+    {
+        
+        static int Max(int a, int b)
+        {
+            return Math.Max(a, b);
+        }
+        static string[] Array(params string[] arr)
+        {
+            string z, x;
+            z = arr[0]; x = arr[1];
+            arr[0] = x; arr[1] = z;
+            return arr;
+            
+        }
+        static uint Factorial(uint value)
+        {
+            uint result = 1;
+            if (value == 0) 
+            {
+            return 1;
+            }
+         
+            else
+            {
+                while (value > 0)
+                {
+                    result = value * result;
+                    value--;
+                }
+            }
+            return result;
+        }
+        static int Nod(int one, int two)
+        {
+            int nod = 0;
+            while(one != 0 &&  two != 0)
+            {
+                if(one > two)
+                {
+                    one = one % two;
+                }
+                else
+                {
+                    two = two % one;
+                }
+            }
+            nod = one + two; 
+            return nod;
+
+        }
+        static int Nod(int one, int two, int three)
+        {
+           int c = Nod(one, two);
+            return Nod(c, three);
+        }
+        static int FibRec(int p1, int p2, int n)
+        {
+            return n == 0 ? p1 : FibRec(p2, p1 + p2, n - 1);
+        }
+        static int Fib(int n) 
+        { 
+            return FibRec(0, 1, n); 
+        }
+        static void Main(string[] args)
+        {
+            // Задание 5.1 Максимальное из чисел
+            Console.WriteLine("Задание 5.1 Максимальное из чисел");
+        Patch:
+            try
+            {
+                Console.WriteLine("Введите первое число");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите второе число");
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine("Максимальное из чисел - " + Max(a, b));
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Повторите попытку и введите целое число");
+                goto Patch;
+            }
+            // Задание 5.2 Поменять местами 2 параметра
+            Console.WriteLine("Задание 5.2 Поменять местами 2 параметра");
+            Console.WriteLine("Введите первый параметр");
+            string q = Console.ReadLine();
+            Console.WriteLine("Введите второй параметр");
+            string w = Console.ReadLine();
+            Console.WriteLine($"{Array(q, w)[0]}  {Array(q, w)[1]}");
+            // Задание 5.3 Факториал числа
+            Console.WriteLine("Задание 5.3 Факториал числа");
+            Console.WriteLine("Введите целое положительное число");
+            Patch1:
+            try
+            {
+                
+                    uint factor = uint.Parse(Console.ReadLine());
+                    uint result = Factorial(factor);
+                if (result == 0)
+                {
+                    Console.WriteLine("False");
+                }
+                else
+                    Console.WriteLine(result);
+                
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Повторите попытку и введите целое число");
+                goto Patch1;
+            }
+            catch(System.OverflowException)
+            {
+               Console.WriteLine("False");
+            }
+            // Задание 5.4 Рекурсивный метод выч. факториала
+            Console.WriteLine(" Задание 5.4 Рекурсивный метод выч. факториала");
+            int Factoriall(int n)
+            {
+                if (n == 1) return 1;
+                return n * Factoriall(n - 1);
+            }
+            Patch2:
+            try
+            {
+                Console.WriteLine("Введите целое положительное число");
+                int factori = int.Parse(Console.ReadLine());
+                factori = Factoriall(factori);
+                if(factori == 0)
+                {
+                    Console.WriteLine("Слишком большое число");
+                }
+                else
+                {
+                    Console.WriteLine(factori);
+                }
+                
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Повторите попытку и введите целое число");
+                goto Patch2;
+            }
+            // Д/З 5.1 Алгоритм евклида для 2 чисел
+            Console.WriteLine("Д/З 5.1 Алгоритм евклида для 2 чисел");
+            Patch3:
+            try
+            
+            {
+                Console.WriteLine("Введите первое число");
+                int one = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите второе число");
+                int two = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Нод = {Nod(one, two)}");
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Повторите попытку и введите целое число");
+                goto Patch3;
+            }
+            // Д/З 5.1 Алгоритм евклида для 3 чисел
+            Console.WriteLine("Д/З 5.1 Алгоритм евклида для 3 чисел");
+            Patch4:
+            try
+            {
+                Console.WriteLine("Введите первое число");
+                int on = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите второе число");
+                int tw = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите третье число");
+                int thr = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Нод = {Nod(on, tw, thr)}");
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Повторите попытку и введите целое число");
+                goto Patch4;
+            }
+            // Д/З 5.2 Число Фибоначчи
+            Console.WriteLine("Д/З 5.2 Число Фибоначчи");
+            Patch5:
+            try
+            {
+                Console.WriteLine("Введите порядковый номер нужного вам числа Фибоначчи");
+                int fibo = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Ваше число Фибоначчи с порядковым номером {fibo} имеет значение = {Fib(fibo)}");
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Повторите попытку и введите целое число");
+                goto Patch5;
+            }
+
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ Practice_30.09.23/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_30._09._23
+{
+    internal class Program
+    {
+        static int Sum(params int[] array)
+        {
+            int result = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += array[i];
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            // Задание 1 Массив из 20 элементов
+            Console.WriteLine("Задание 1 Массив из 20 элементов");
+            int[] arr = new int[20];
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next();
+            }
+            foreach (int i in arr)
+                Console.WriteLine(i);
+            Patch1:
+            try
+            {
+                Console.WriteLine("Введите первое число, которое хотите поменять местами ");
+                int replace1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите второе число, которое хотите поменять местами ");
+                int replace2 = int.Parse(Console.ReadLine());
+                int index1 = Array.IndexOf(arr, replace1);
+                int index2 = Array.IndexOf(arr, replace2);
+                arr[index1] = arr[index2];
+                arr[index2] = replace1;
+                Console.WriteLine("Изменённый массив ");
+                foreach (int i in arr)
+                    Console.WriteLine(i);
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                Console.WriteLine("Вы ввели не существующее в данном массиве число");
+                goto Patch1;
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Введите число");
+                goto Patch1;
+            }
+            // Задание 2 Массив, произведение и среднее арифмет.
+            Console.WriteLine("Задание 2 Массив, произведение и среднее арифмет.");
+            int result = Sum(3, 6, 9, 15, 27);
+            int temp = 1;
+            int a = 5;
+            int[] ints = new int[a];
+            ints[0] = 3;
+            ints[1] = 6;
+            ints[2] = 9;
+            ints[3] = 15;
+            ints[4] = 27;
+            foreach (int i in ints)
+                temp *= i;
+
+            Console.WriteLine($"Сумма элементов массива = {result}");
+            Console.WriteLine($"Произведение элементов массива = {temp}");
+            temp = 0;
+            foreach (int i in ints)
+                temp += i;
+            temp = temp / a;
+            Console.WriteLine($"Среднее арифмет. элементов массива = {temp}");
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+     
+}
